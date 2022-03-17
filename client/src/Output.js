65efc4b7c38273ee331535a875b6d442237cfbf9
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2022-03-17 10:36:12 +0545

    output copying functionality added

--- client/src/Output.js
+++ client/src/Output.js
@@ -3,19 +3,28 @@ import { Form, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Output(props) {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(props.formatted_equation);
+  };
   return (
-    <div className="offset-md-1">
-      <Form>
-        <Form.Group as={Row} controlId="output1">
-          <Form.Label column sm="3">
-            Recognized Expression:-
-          </Form.Label>
-          <Col sm="5">
-            <Form.Control readOnly placeholder={props.formatted_equation} />
-          </Col>
-        </Form.Group>
-      </Form>
-    </div>
+    <>
+      <div className="offset-md-1">
+        <Form>
+          <Form.Group as={Row} controlId="output1">
+            <Form.Label column sm="3">
+              Recognized Expression:-
+            </Form.Label>
+            <Col sm="5">
+              <Form.Control readOnly placeholder={props.formatted_equation} />
+            </Col>
+          </Form.Group>
+        </Form>
+      </div>
+
+      <button className="btn btn-primary mx-2" onClick={handleCopy}>
+        Copy Output
+      </button>
+    </>
   );
 }
 
